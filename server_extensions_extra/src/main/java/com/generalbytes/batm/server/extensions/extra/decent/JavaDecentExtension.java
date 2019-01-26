92b9b8c587f5532bc9e95988182cd9800f747466
Unknown <peter.nosko@gmail.com>
2019-01-26 01:26:56 +0100

    Adapted to new API

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/decent/JavaDecentExtension.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/decent/JavaDecentExtension.java
@@ -1,4 +1,11 @@
 package com.generalbytes.batm.server.extensions.extra.decent;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class JavaDecentExtension extends DecentExtension {
+    @Override
+    public Set<Class> getChatCommands() {
+        return new HashSet<Class>() ;
+    }
 }
